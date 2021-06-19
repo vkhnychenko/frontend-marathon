30619af284b3ef701c5c692a7e9c4da4b216262d
vkhnychenko <khnychenko96@gmail.com>
2021-06-19 15:24:44 +0300

    add new lessons

---
+++ 04-board-sources/app.js
@@ -0,0 +1,31 @@
+const board = document.querySelector('#board')
+const colors = ['#7CFC00', '#00FF00', '#32CD32', '#98FB98', '#00FA9A']
+const SQUARES_NUMBER = 500
+
+for (let i =0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+
+    square.addEventListener('mouseleave', () => removeColor(square))
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
